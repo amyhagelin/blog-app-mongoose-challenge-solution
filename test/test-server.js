72a3aa811bb0a1e59553925d0fe4fcab91838bb2
amyhagelin <ahagelin@gmail.com>
2017-02-28 21:53:34 -0800

    final corrections to test delete and put

--- test/test-server.js
+++ test/test-server.js
@@ -30,7 +30,7 @@ function seedBlogPostData() {
 			author: {
 				firstName: faker.name.firstName(),
 				lastName: faker.name.lastName()
-			}
+			},
 			title: faker.lorem.sentence(),
 			content: faker.lorem.text()
 		});
@@ -46,89 +46,114 @@ describe('blog posts API resource', function() {
 		return seedBlogPostData();
 	});
 	afterEach(function() {
-		retrn tearDownDb();
+		return tearDownDb();
 	});
 	after(function() {
 		return closeServer();
 	})
+
+	it('should return all blog posts', function() {
+		let res;
+		return chai.request(app)
+			.get('/posts')
+			.then(function(_res) {
+				console.log('response')
+				res = _res;
+				res.should.have.status(200);
+				// console.log(res.body.length, count)
+				res.body.should.have.length.of(10);
+			})
+	});
+
+	function generatePost() {
+		return {
+			author: {
+				firstName: faker.name.firstName(),
+				lastName: faker.name.lastName()
+			},
+			title: faker.lorem.sentence(),
+			content: faker.lorem.text()
+		}
+	}
+	// POST request
+	const newPost = generatePost() // where does this come from?
+
+	it('should create a new blog post', function() {
+		return chai.request(app)
+			.post('/posts')
+			.send(newPost)
+			.then(function(res){
+				res.should.have.status(201);
+				res.should.be.json;
+				res.body.should.be.a('object');
+				res.body.should.include.keys('title', 'content', 'author');
+				res.body.title.should.equal(newPost.title);
+				res.body.id.should.not.be.null;
+				res.body.content.should.equal(newPost.content);
+				res.body.author.should.equal(`${newPost.author.firstName} ${newPost.author.lastName}`.trim());
+				return BlogPost.findById(res.body.id);
+			})
+			.then(function(blogPost) {
+				blogPost.title.should.equal(newPost.title);
+				blogPost.content.should.equal(newPost.content);
+				// blogPost.author.should.equal(newPost.author);
+			});
+	})
+
+
+	// PUT request
+	const updateData = {title: 'Updated Title'};
+
+	it('should update an existing blog post', function() {
+		return BlogPost
+		.findOne()
+		.exec()
+		.then(function(blogPost){
+			updateData.id = blogPost.id;
+			return chai.request(app)
+				.put(`/posts/${blogPost.id}`)
+				.send(updateData);
+		})
+		.then(function(res){
+			res.should.have.status(201);
+
+			return BlogPost.findById(updateData.id).exec();
+		})
+		.then(function(blogPost) {
+			blogPost.title.should.equal(updateData.title);
+		});
+	})
+
+	it('should delete a blog post', function() {
+
+		// DELETE request
+		let blogPostTemp;
+
+		return BlogPost
+			.findOne()
+			.exec()
+			.then(function(blogPost) {
+				blogPostTemp = blogPost;
+				return chai.request(app).delete(`/posts/${blogPost.id}`);
+			})
+			.then(function(res){
+				res.should.have.status(204);
+				return BlogPost.findById(blogPostTemp.id).exec();
+			})
+			.then(function(blogPost){
+				should.not.exist(blogPost);
+			})	
+	})
+
+
 });
 
 
 // BELOW is done by me, ABOVE was from solution
 
 // GET request
-it('should return all blog posts', function() {
-	let res;
-	return chai.request(app)
-		.get('/posts')
-		.then(function(_res) {
-			res = _res;
-			res.should.have.status(200);
-			res.body.posts.should.have.length.of(count);
-		});
-});
 
-// POST request
-const newPost = generatePost() // where does this come from?
 
-return chai.request(app)
-	.post('/posts')
-	.send(newPost)
-	.then(function(res)){
-		res.should.have.status(201);
-		res.should.be.json;
-		res.body.should.be.a('object');
-		res.body.should.include.keys('title', 'content', 'author');
-		res.body.title.should.equal(newPost.title);
-		res.body.id.should.not.be.null;
-		res.body.content.should.equal(newPost.content);
-		res.body.author.should.equal(newPost.author);
-		return BlogPost.findById(res.body.id);
-	})
-	.then(function(blogPost)) {
-		blogPost.title.should.equal(newPost.title);
-		blogPost.content.should.equal(newPost.content);
-		blogPost.author.should.equal(newPost.author);
-	});
-
-// PUT request
-const updateData = {title: 'Updated Title'};
-
-return BlogPost
-	.findOne()
-	.exec()
-	.then(function(blogPost){
-		updateData.id = restaurant.id;
-		return chai.request(app)
-			.put(`/posts/$[blogPost.id]`)
-			.send(updateData);
-	})
-	.then(function(res){
-		res.should.have.status(204);
-
-		return BlogPost.findByID(updateData.id).exec();
-	})
-	.then(function(blogPost) {
-		blogPost.title.should.equal(updateData.title);
-	});
-
-	// DELETE request
-	let blogPost;
-
-	BlogPost
-		.findOne()
-		.exec()
-		.then(function(_blogPost) {
-			blogPost = _blogPost;
-			return.chai.request(app).delete(`/posts/$[blogPost.id]`);
-		})
-		.then(function(res){
-			res.should.have.status(204);
-			return BlogPost.findById(blogPost.id).exec();
-		})
-		.then(function(_blogPost){
-			should.not.exist(_blogPost);
-		})
 
 
 
